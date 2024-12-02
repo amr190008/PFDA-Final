f5bb8f0d032e94ff73153e839ba065ca615fdbaf
Anthony Romero <amr190008@utdallas.edu>
2024-12-02 03:05:26 -0600

    Added a method that sets up pur audio stream

--- src/project.py
+++ src/project.py
@@ -5,3 +5,13 @@ import pygame
 # Our Constants
 CHUNK = 1024               # Number of audio samples per frame
 RATE = 44100               # Sampling rate in Hertz / Hz
+
+# This is to set up the audio stream
+def init_audio():
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16,
+                    channels=1,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+    return stream, p
