0e25c6c341adb6bf1791445215c364c3bbc83d89
Anthony Romero <amr190008@utdallas.edu>
2024-12-03 09:11:02 -0600

    Added Visualizer class and animations to Project

--- src/project.py
+++ src/project.py
@@ -21,3 +21,21 @@ def get_audio_data(stream):
     data = np.frombuffer(stream.read(CHUNK), dtype=np.int16)
     volume = np.abs(data).mean()
     return volume
+
+
+# Visualizer class to handle animations and rendering
+class SoundVisualizer:
+    def __init__(self, screen):
+        self.screen = screen
+        self.width, self.height = screen.get_size()
+
+
+def draw_bars(self, volume):
+        color_intensity = min(255, int(volume / 100))    # Normalize volume for color
+        color = (color_intensity, 0, 255 - color_intensity)
+        bar_width = int(self.width / 20)                 # Set number of bars
+        for i in range(20):
+            bar_height = int(volume / 1000)              # Scale the bar height with volume
+            x = i * bar_width
+            y = self.height - bar_height
+            pygame.draw.rect(self.screen, color, (x, y, bar_width - 2, bar_height))
